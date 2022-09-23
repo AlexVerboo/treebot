a6d917a63b2ef7dd87832f653f669938b5e6c0b8
AlexVerboo <alex.verboonen@hotmail.com>
2022-09-23 12:40:18 -0500

    Doce

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import groupy
 from groupy.client import Client as groupmeClient
 from urllib.request import Request, urlopen
 from flask import Flask, request
-
 app = Flask(__name__)
-
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
