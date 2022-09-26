2900fdd2d4e7b5b5a45673f70e5feb7151c53811
AlexVerboo <alex.verboonen@hotmail.com>
2022-09-25 23:12:12 -0500

    Veintiuno

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def webhook():
   data['text'] = data['text'].lower()
   log('Recieved {}'.format(data))
 
-  byebye = ["night-night", "sweet dreams, I love you", "Bye Fucker","Adios","Yeah, go away already. Everyone!, They're gone, lets play!"]
+  byebye = ["night-night "+data['name'], "sweet dreams, I love you "+data['name'], "Bye Fucker","Adios "+data['name'],"Yeah, go away already. Everyone!, "+data['name']+" is gone, lets play!"]
   hihi = ["Hello", "Yes?", "Hola","hu?",data['name']+"?"]
   # We don't want to reply to ourselves!
   if data['name'] != 'Wild Palm Tree':
