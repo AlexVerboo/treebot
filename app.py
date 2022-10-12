3ca74a32f2322a56809c18a43c0acd7b7c81b70f
AlexVerboo <alex.verboonen@hotmail.com>
2022-10-11 23:07:05 -0500

    Veintisiete

--- app.py
+++ app.py
@@ -152,8 +152,8 @@ def PersonalRecords(nombre):
   statsmatiz = client.open("Trees in space game Records").worksheet('Trees in Space Members').get("C2:H19")
   for x in statsmatiz:
       if x[0]==usergamertag:
-          output +="This are the stats for " +usergamertag+"\n"
+          output +="This are the stats for " +nombre+"\n"
           for y in range(len(x)):
-              output +=  statsmatiz[0][y]+ " =>  "+x[y]+"\n"
+              output +=  statsmatiz[0][y]+ " 🡺  "+x[y]+"\n"
   if output=="" : output+="I dont see your name on the Stats list. Tell my boss to update his shit....  NEXT!!!"
   return (output)
